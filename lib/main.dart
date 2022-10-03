2b94e4c5a6fc2d9ba214e2fd7ad5d944c84ab642
Juan José <jujopere@bancolombia.com.co>
2022-10-03 10:57:22 -0500

    main base

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:cats_gif/ui/view/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Creamos un "provider", el cual almacenará un valor (aquí "Hello world").
-// El usar un provider nos permitirá simular/sobrescribir el valor expuesto.
-final helloWorldProvider = Provider((_) => 'Hello Cats');
-
 void main() {
   runApp(
     // Para que los widgets puedan leer los providers, necesitamos envolver
